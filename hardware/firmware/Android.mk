6503dd032a9cd65068c4e7beace6876ef90edeb6
electimon <electimon@gmail.com>
2022-01-08 22:23:56 +0000

    common: hardware/firmware: Create /vendor/fsg as well
    * Needed for rmt_storage
    
    Signed-off-by: electimon <electimon@gmail.com>

--- hardware/firmware/Android.mk
+++ hardware/firmware/Android.mk
@@ -8,6 +8,7 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)
 
 LOCAL_POST_INSTALL_CMD := mkdir -p \
     $(TARGET_OUT_VENDOR)/dsp \
+    $(TARGET_OUT_VENDOR)/fsg \
     $(TARGET_OUT_VENDOR)/firmware_mnt \
     $(TARGET_OUT_VENDOR)/bt_firmware
 
