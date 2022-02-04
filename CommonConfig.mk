216e1c440c2d9e0763c53c47e338a3080543cd78
electimon <electimon@gmail.com>
2022-02-04 19:25:40 +0000

    common: Remove old Sony packages and references
    Signed-off-by: electimon <electimon@gmail.com>

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
--include device/sony/customization/Customization.mk
-
 # Fixes
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
