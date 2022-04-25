8680259a39973173d91a4cbd822d5b49de7682dc
Sal Savage <salsavage@google.com>
2022-04-25 16:43:16 -0700

    common: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is
    correct
    
    Change-Id: I24fd57bacbf6786a26f7079e7a6e9857dbe170d1

--- common-prop.mk
+++ common-prop.mk
@@ -206,6 +206,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.profile.tbs.server.enabled=true \
     bluetooth.profile.vc.server.enabled=true
 
+# Set the Bluetooth Class of Device
+# Service Field: 0x5A -> 90
+#    Bit 17: Networking
+#    Bit 19: Capturing
+#    Bit 20: Object Transfer
+#    Bit 22: Telephony
+# MAJOR_CLASS: 0x02 -> 2 (Phone)
+# MINOR_CLASS: 0x0C -> 12 (Smart Phone)
+PRODUCT_PRODUCT_OVERRIDES += \
+    bluetooth.device.class_of_device=90,2,12
+
 # Disable Compressed APEX on 4.14 kernel as Android 12 enforces it and our kernel is not compatible (yet)
 ifeq ($(KERNEL_VERSION), 4.14)
 OVERRIDE_PRODUCT_COMPRESSED_APEX := false
