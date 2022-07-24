33e0fe3f15a38200fcddd180ea56e84a9729c57e
electimon <electimon@gmail.com>
2022-07-24 06:09:29 +0000

    common: prop: Enable FUSE passthrough
    Signed-off-by: electimon <electimon@gmail.com>

--- common-prop.mk
+++ common-prop.mk
@@ -178,6 +178,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PRODUCT_OVERRIDES += \
     ro.support_one_handed_mode=true
 
+# Use FUSE passthrough
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
+
 # Disable Compressed APEX on 4.14 kernel as Android 12 enforces it and our kernel is not compatible (yet)
 ifeq ($(KERNEL_VERSION), 4.14)
 OVERRIDE_PRODUCT_COMPRESSED_APEX := false
