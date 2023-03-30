b99d8bb21d5626ed8bde0161ebfbd4b78fbca0a9
betaxab <betaxab@gmail.com>
2023-03-31 01:05:31 +0800

    common: hardware/qcom: Include utils.mk for board_config as well
    * Some inheritors would like to use some utils
    
    Signed-off-by: betaxab <betaxab@gmail.com>

--- hardware/qcom/board.mk
+++ hardware/qcom/board.mk
@@ -6,5 +6,6 @@ TARGET_USES_ION := true
 
 # QCOM Common Board Hook
 ifneq ($(ROM_INCLUDES_QCOM_COMMON),true)
+    include device/motorola/common/hardware/qcom/utils.mk
     include device/qcom/common/BoardConfigQcom.mk
 endif
