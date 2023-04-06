ae5cff95006e97c0c5f70f67a5d5b97ff23532f2
electimon <electimon@gmail.com>
2023-04-06 05:20:40 +0000

    common: rootdir: Introduce init.modprobe.sh
    * Loads all modules in /vendor/lib/modules
      when modules.load is absent.
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I570e21b3a5be4c3701e289cd47d80f080a4dd0df

---
+++ rootdir/vendor/bin/init.modprobe.sh
@@ -0,0 +1,15 @@
+#!/vendor/bin/sh
+if [ -f "/vendor/lib/modules/modules.load" ]; then
+    exit 0
+fi
+
+MODULES_PATH="/vendor/lib/modules/"
+MODULES=`/vendor/bin/modprobe -d ${MODULES_PATH} -l`
+
+# Iterate over module list and modprobe them in background.
+for MODULE in ${MODULES}; do
+	/vendor/bin/modprobe -a -b -d ${MODULES_PATH} ${MODULE} &
+done
+
+# Wait until all the modprobes are finished
+wait
