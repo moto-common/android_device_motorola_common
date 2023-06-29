2ca4498e184282f0572fb123d159f007a5a632ac
electimon <electimon@gmail.com>
2023-06-29 13:14:39 +0000

    common: Add widevine service
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I01b22106ca99390dfba08b505623c5f52705e95a

--- common-treble.mk
+++ common-treble.mk
@@ -50,6 +50,10 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0.vendor \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3.vendor \
     android.hardware.drm-service-lazy.clearkey
 
 # Fingerprint
