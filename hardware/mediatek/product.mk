3da2e2a2f1e167dc449112458bc1bae91fe1855e
electimon <electimon@gmail.com>
2023-08-05 07:49:03 +0800

    common: hardware/mediatek: Set kernel image name
    * Needs to be Image.gz always
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: Ibf1e30da56c4a11ef9aab8bcc6f1e0a0efb12f19

--- hardware/mediatek/product.mk
+++ hardware/mediatek/product.mk
@@ -2,5 +2,9 @@
 TARGET_COMMON_MTK_COMPONENTS := all
 include device/mediatek/common/common.mk
 
+# Kernel
+## The bootloader will not accept anything but Image.gz
+TARGET_KERNEL_IMAGE_NAME := Image.gz
+
 # Hardware
 PRODUCT_SOONG_NAMESPACES += hardware/mediatek
